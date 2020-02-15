ce74695c89a4fc0a23af17390d8b96b1880014e6
rwowensv2 <rwowensv2@outlook.com>
2020-02-15 10:28:59 -0500

    terraform: portainer

---
+++ portainer/portainer.tf
@@ -0,0 +1,21 @@
+provider "docker" {}
+resource "docker_container" "portainer" {
+  name = "portainer"
+  image = "portainer/portainer:latest"
+
+  ports {
+    internal = "9000"
+    external = "9000"
+  }
+ 
+  ports {
+    internal = "8000"
+    external = "8000"
+  }
+
+  volumes {
+    volume_name = "portainer_data"
+    container_path = "/data"
+  }
+
+}
