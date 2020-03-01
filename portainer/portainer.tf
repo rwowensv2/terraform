2d5ce4ccb34f7738d4ea03e25951810e6a96916d
Rob Owens <rwowensv2@outlook.com>
2020-02-29 22:38:00 -0500

    portainer

--- portainer/portainer.tf
+++ portainer/portainer.tf
@@ -5,22 +5,7 @@ resource "docker_service" "portainer" {
   task_spec {
     container_spec {
       image = "portainer/portainer:latest"
-
-      mounts = [
-        {
-          target    = "/data"
-          source    = "portainer_data"
-          type      = "volume"
-          read_only = false
-        },
-      ]
     }
-  }
-
-  placement {
-   constraints = [
-      "node.role==manager",
-    ]
   }
   
   endpoint_spec {
