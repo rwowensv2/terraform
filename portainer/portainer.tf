865605added0a6510b673a7400204a8f06327aa2
rwowensv2 <rwowensv2@outlook.com>
2020-03-01 07:48:58 -0500

    portainer:  dock syntax out of date.

--- portainer/portainer.tf
+++ portainer/portainer.tf
@@ -1,12 +1,23 @@
 provider "docker" {}
+
+resource "docker_volume" "portainer_volume" {
+  name = "portainer_data"
+}
+
 resource "docker_service" "portainer" {
   name = "portainer-service"
 
   task_spec {
     container_spec {
       image = "portainer/portainer:latest"
-    }
     
+      mounts {
+          target    = "/data"
+          source    = "portainer_data"
+          type      = "volume"
+      }
+    }
+
     placement {
       constraints = ["node.role==manager"]
     }
